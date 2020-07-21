4c038a06b9c01d979d20b7eb20824175e8b14d52
Ludo Loops <ludoloops@prontonmail.com>
2020-07-22 01:41:00 +0200

    Feat: 51 managing data

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.css']
+  styleUrls: ['./tabs.component.css'],
 })
 export class TabsComponent implements OnInit {
+  characters = [
+    { name: 'Luke Skywalker', side: '' },
+    { name: 'Darth Vader', side: '' },
+  ];
+  chosenList = 'all';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onChoose(side) {
+    this.chosenList = side;
   }
-
+  getCharacters() {
+    if (this.chosenList === 'all') {
+      return this.chosenList.slice();
+    }
+    return this.characters.filter((char) => {
+      return char.side === this.chosenList;
+    });
+  }
 }
